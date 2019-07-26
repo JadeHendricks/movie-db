1f0de220fdc4a2aef314569c97c64bd1758e5f2b
jade <hendricksjade@gmail.com>
2019-07-26 22:48:08 +0200

    Adding in the link depending on the id to watchVideo

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 
 const Header = ({ movie: { backdrop_path, title, overview, release_date, original_language, vote_average, revenue, id } }) => {
+
+  const API_KEY = "e87f29ad6137f88242f3bcd9b94b1af7";
+  const [video, setVideo] = useState("");
 
   const headerBackgroundImage = {
     backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(https://image.tmdb.org/t/p/original${backdrop_path})`,
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center center",
     backgroundSize: "cover"
+  }
+
+  useEffect(() => {
+    fetchVideos(id);
+  }, [id]);
+
+  const fetchVideos = async (id) => {
+    if(!id) return;
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US&page=1`);
+    const data = await response.json();
+    setVideo(data.results[0].key);
+  }
+
+  const watchVideo = (e) => {
+    e.target.setAttribute("href", `https://www.youtube.com/watch?v=${video}`);
   }
 
   return (
@@ -32,7 +50,7 @@ const Header = ({ movie: { backdrop_path, title, overview, release_date, origina
               </li>
             }
             <li className="headerMainMovie__li">
-              <button className="button button--white">Watch now</button>
+              <Link className="button button--white" target="_blank" onClick={watchVideo} to={""}>Watch now</Link>
             </li>
           </ul>
         </div>
